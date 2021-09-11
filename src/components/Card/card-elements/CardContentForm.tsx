cddb4183c97674975280f7935eefc8e4e2236c22
Jakub Topoliński <topolinskijakub5@gmail.com>
2021-09-11 11:05:58 +0200

    Text now gets smaller every 30 typed characters

--- src/components/Card/card-elements/CardContentForm.tsx
+++ src/components/Card/card-elements/CardContentForm.tsx
@@ -1,6 +1,7 @@
 import { CardEditEntity } from '../../../data/card'
 
 import { useInput } from '../../../hooks/useInput'
+import { useEffect, useState } from 'react'
 
 import styles from './CardContentForm.module.css'
 
@@ -11,19 +12,27 @@ interface CardContentFormProps {
   onSubmit(values: CardContentFormValues): void
 }
 
+const DEFAULT_FONT_SIZE = 16;
+
 export const CardContentForm = (props: CardContentFormProps) => {
   const { value, handleChange } = useInput(props.initialValues.content)
+  const [fontSize, setFontSize] = useState(DEFAULT_FONT_SIZE);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     props.onSubmit({ ...props.initialValues, content: value })
   }
 
+  useEffect(() => {
+    setFontSize(DEFAULT_FONT_SIZE - Math.floor(value.length / 30));
+  }, [setFontSize, value]);
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
         className={styles.textarea}
         autoFocus
+        style={{fontSize: `${fontSize}px`}}
         value={value}
         onBlur={handleSubmit}
         onChange={handleChange}
